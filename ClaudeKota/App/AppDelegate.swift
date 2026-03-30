30f1be37fb04387c9b6f7e44b704db9fb6cfca73
BatuhanAkpunar <batuhan.akpunar@gmail.com>
2026-03-31 00:25:38 +0300

    chore: scaffold ClaudeKota project with XcodeGen

---
+++ ClaudeKota/App/AppDelegate.swift
@@ -0,0 +1,11 @@
+import AppKit
+
+@MainActor
+final class AppDelegate: NSObject, NSApplicationDelegate {
+    private var statusItem: NSStatusItem?
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        statusItem?.button?.title = "CK"
+    }
+}
